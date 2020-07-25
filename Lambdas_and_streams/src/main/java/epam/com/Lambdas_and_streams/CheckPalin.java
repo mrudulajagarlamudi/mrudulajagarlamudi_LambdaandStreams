d0df8e31afaccca03f2bf050e91d7c6fe3ae42b5
nikitha02 <18wh5a0502@bvrithyderabad.edu.in>
2020-07-25 13:09:40 +0530

    committed a lambads  and streams

---
+++ Lambdas_and_streams/src/main/java/epam/com/Lambdas_and_streams/CheckPalin.java
@@ -0,0 +1,38 @@
+package epam.com.Lambdas_and_streams;
+
+
+import java.util.*;
+import java.util.function.Predicate;
+
+class Palindrome {
+	public static boolean isPalindrome(String str) {
+		StringBuilder s = new StringBuilder(str);
+		s.reverse();
+		return s.toString().equals(str);
+	}
+}
+
+public class CheckPalin {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<String> strings  = new ArrayList<String>();
+		System.out.println("Enter the number of elements in the list");
+		int n = sc.nextInt();
+		System.out.println("Enter the string values");
+		for(int i = 0; i < n; i++) {
+			strings.add(sc.next());
+		}
+		List<String> palindrome = CheckPalin.filterChecks(strings, Palindrome::isPalindrome);
+		System.out.println("Palindrome strings are : " + palindrome);	
+	}
+
+	public static List<String> filterChecks(List<String> strings, Predicate<String> palindromePredicate) {
+		List<String> palindromeList = new ArrayList<String>();
+		for(String str:strings) {
+			if(palindromePredicate.test(str)) {
+				palindromeList.add(str);
+			}
+		} return palindromeList;
+	}
+}
